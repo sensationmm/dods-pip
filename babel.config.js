d08896504b55b843ff5ae4f8e525673f36550aec
Kevin Reynolds <kevin.reynolds@somoglobal.com>
2021-07-20 10:37:51 +0100

    Styled components

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  plugins: ['@babel/plugin-transform-runtime'],
+  plugins: ['@babel/plugin-transform-runtime', ['styled-components', { ssr: true }]],
   presets: ['@babel/preset-env', '@babel/preset-react', 'next/babel'],
 };
